31f5c81187e897b7b6a695f22d836a24ecabe45f
StephanHanzlik <stephenhanzlik@gmail.com>
2020-04-08 08:28:47 -0600

    Updated station with better getters

--- src/main/java/com/enos/model/Station.java
+++ src/main/java/com/enos/model/Station.java
@@ -1,31 +1,45 @@
 package com.enos.model;
 
 public class Station {
-    public int sid;
-    public int elevation;
-    public int latitude;
-    public int longitude;
-    public String name;
-    public int timezone;
-    public String triplet;
-    public boolean wind;
 
-    public String getTriplet() {
-        return triplet;
-    }
+//    codejava.net/coding/java-getter-and-setter-tutorial-from-basics-to-best-practices
+    private int sid;
+    private int elevation;
+    private int latitude;
+    private int longitude;
+    private String name;
+    private int timezone;
+    private String triplet;
+    private boolean wind;
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setElevation(int elevation) {
         this.elevation = elevation;
     }
 
+    public int getElevation() {
+        return elevation;
+    }
+
     public void setWind(boolean wind) {
         this.wind = wind;
     }
 
+    public boolean getWind() {
+        return wind;
+    }
+
     public void setTriplet(String triplet) { this.triplet = triplet; }
+
+    public String getTriplet() {
+        return triplet;
+    }
 
 }
