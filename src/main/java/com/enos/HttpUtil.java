0e26f58185b23809b4ca04424dfe1025168bc2f9
StephanHanzlik <stephenhanzlik@gmail.com>
2020-04-09 08:27:50 -0600

    began adding snotel reports service

--- src/main/java/com/enos/HttpUtil.java
+++ src/main/java/com/enos/HttpUtil.java
@@ -13,44 +13,17 @@ import java.io.IOException;
 
 public class HttpUtil extends HttpServlet {
 
-    public void doGet() throws IOException {
+    public String doGet() throws IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
 
         try {
-//**********************************************************************************
-//            Old Call: http://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customSingleStationReport/daily/335:CO:SNTL%7Cid%3D%22%22%7Cname/2013-01-15,2013-01-18/WTEQ%3A%3Avalue%2CWTEQ%3A%3Adelta%2CSNWD%3A%3Avalue%2CSNWD%3A%3Adelta
-//
-//            WTEQ::value,WTEQ::delta,SNWD::value,SNWD::delta
-//
-//            Date: 2013-01-18
-//            Snow Water Equivalent (in) Start of Day Values: 5.8
-//            Change In Snow Water Equivalent (in): 0.0
-//            Snow Depth (in) Start of Day Values: 28
-//            Change In Snow Depth (in): -1
-//
-//
-//            New Call: https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customMultiTimeSeriesGroupByStationReport/daily/start_of_period/335:CO:SNTL%7Cid=%22%22%7Cname/0,0/TAVG::value,TMAX::value,TMIN::value,PREC::value,PRCP::value,SNWD::value,TOBS::value?fitToScreen=false
-//
-//            TAVG::value,TMAX::value,TMIN::value,PREC::value,PRCP::value,SNWD::value,TOBS::value
-//
-//            Date: 2020-04-08
-//            Air Temperature Average (degF): Null
-//            Air Temperature Maximum (degF): Null
-//            Air Temperature Minimum (degF): Null
-//            Precipitation Accumulation (in) Start of Day Values: 23.8
-//            Precipitation Increment (in): Null
-//            Snow Depth (in) Start of Day Values: 64
-//            Air Temperature Observed (degF) Start of Day Values: 29
-// **********************************************************************************
+//          https://httpbin.org/get
 
-
-
-
-            HttpGet request = new HttpGet("https://httpbin.org/get");
+            HttpGet request = new HttpGet("http://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customSingleStationReport/daily/335:CO:SNTL%7Cid%3D%22%22%7Cname/2013-01-15,2013-01-18/WTEQ%3A%3Avalue%2CWTEQ%3A%3Adelta%2CSNWD%3A%3Avalue%2CSNWD%3A%3Adelta");
 
             // add request headers
-            request.addHeader("custom-key", "mkyong");
-            request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
+//            request.addHeader("custom-key", "mkyong");
+//            request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
 
             CloseableHttpResponse response = httpClient.execute(request);
 
@@ -67,6 +40,10 @@ public class HttpUtil extends HttpServlet {
                     // return it as a String
                     String result = EntityUtils.toString(entity);
                     System.out.println(result);
+                    return result;
+                } else {
+                    //Will want to add exception handling here
+                    return "502";
                 }
 
             } finally {
---
+++ src/main/java/com/enos/SnotelReportsService.java
@@ -0,0 +1,45 @@
+package com.enos;
+
+
+//a service to build requests to pass to HttpUtil and to format data
+public class SnotelReportsService {
+
+    public static final String BASE_URL = "https://wcc.sc.egov.usda.gov/reportGenerator";
+    public static final String CSV_FORMAT = "/view_csv";
+    public static final String CUSTOM_SINGLE_STATION_REPORT= "/customSingleStationReport";
+    public static final String CUSTOM_MULTI_TIME_SERIES_GROUP_BY_STATION_REPORT = "/customMultiTimeSeriesGroupByStationReport";
+
+
+//**********************************************************************************
+//            Old Call: http://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customSingleStationReport/daily/335:CO:SNTL%7Cid%3D%22%22%7Cname/2013-01-15,2013-01-18/WTEQ%3A%3Avalue%2CWTEQ%3A%3Adelta%2CSNWD%3A%3Avalue%2CSNWD%3A%3Adelta
+//
+//            WTEQ::value,WTEQ::delta,SNWD::value,SNWD::delta
+//
+//            Date: 2013-01-18
+//            Snow Water Equivalent (in) Start of Day Values: 5.8
+//            Change In Snow Water Equivalent (in): 0.0
+//            Snow Depth (in) Start of Day Values: 28
+//            Change In Snow Depth (in): -1
+//
+//
+//            New Call: https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customMultiTimeSeriesGroupByStationReport/daily/start_of_period/335:CO:SNTL%7Cid=%22%22%7Cname/0,0/TAVG::value,TMAX::value,TMIN::value,PREC::value,PRCP::value,SNWD::value,TOBS::value?fitToScreen=false
+//
+//            TAVG::value,TMAX::value,TMIN::value,PREC::value,PRCP::value,SNWD::value,TOBS::value
+//
+//            Date: 2020-04-08
+//            Air Temperature Average (degF): Null
+//            Air Temperature Maximum (degF): Null
+//            Air Temperature Minimum (degF): Null
+//            Precipitation Accumulation (in) Start of Day Values: 23.8
+//            Precipitation Increment (in): Null
+//            Snow Depth (in) Start of Day Values: 64
+//            Air Temperature Observed (degF) Start of Day Values: 29
+// **********************************************************************************
+
+
+
+    public String buildSnotelReportUrl(){
+        return "a new url";
+    }
+
+}
