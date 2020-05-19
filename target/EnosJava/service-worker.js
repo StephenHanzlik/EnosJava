b4760f8c9490a8a2be49f81bbeb706df25e2dcf8
StephanHanzlik <stephenhanzlik@gmail.com>
2020-05-18 18:10:23 -0600

    Have front end building as jar and showing in UI

--- target/EnosJava/service-worker.js
+++ target/EnosJava/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/EnosJava/precache-manifest.15ceac55d7f8728196f6925a848c6e3b.js"
+  "/precache-manifest.ddd2eb960e9d0453bb9a48547cefa0f7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/EnosJava/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
